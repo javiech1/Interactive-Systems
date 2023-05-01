31ab3d62e47097fad3f8610d0a6a6cb903bd1854
javiech1 <92743651+javiech1@users.noreply.github.com>
2023-05-01 18:16:00 +0200

    part 1 lab 2

---
+++ Lab2_Scripting_205574/Introduction to Scripting Part 1/Assets/RW/Scripts/hayMachine.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hayMachine : MonoBehaviour
+{
+    public float movementSpeed;
+    public float horizontalBoundary = 22;
+    public GameObject hayBalePrefab;
+    public Transform haySpawnPoint;
+    public float shootInterval;
+    private float shootTimer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    // Update is called once per frame
+    void Update() 
+    {
+        UpdateMovement();
+        UpdateShooting();
+    }
+    private void UpdateMovement()
+    {
+
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        
+        if(horizontalInput < 0 && transform.position.x > -horizontalBoundary){
+            
+            transform.Translate(transform.right * -movementSpeed * Time.deltaTime);
+        
+        }
+        
+        else if(horizontalInput > 0 && transform.position.x <  horizontalBoundary){
+            
+            transform.Translate(transform.right * movementSpeed * Time.deltaTime);
+       
+        }
+    }
+
+    private void UpdateShooting()
+    {
+        shootTimer -= Time.deltaTime;
+
+        if(shootTimer <= 0 && Input.GetKey(KeyCode.Space))
+        {
+            shootTimer = shootInterval;
+            ShootHay();
+        }
+    }
+
+    private void ShootHay()
+    {
+        Instantiate(hayBalePrefab, haySpawnPoint.position, Quaternion.identity);
+    }
+
+}
